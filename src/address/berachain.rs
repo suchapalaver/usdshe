4c3690bf07f2ac694e02105254417d326c58d536
Joseph Livesey <joseph@semiotic.ai>
2025-10-21 16:18:41 -0400

    fix: fix berachain USDC.e address

--- src/address/berachain.rs
+++ src/address/berachain.rs
@@ -1,2 +1,2 @@
-/// <https://berascan.com/address/0x0d05a7d3448512b78fa8a9e46c4872c88c4a0d05>
-pub const USDC: &str = "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d";
+/// <https://berascan.com/address/0x549943e04f40284185054145c6E4e9568C1D3241>
+pub const USDC: &str = "0x549943e04f40284185054145c6E4e9568C1D3241";
